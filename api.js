dc53c11356806efa8511dc9d9263b628723df5d3
clarababette <clara_coetzee@zoho.com>
2022-04-22 14:18:22 +0200

    travis test

--- api.js
+++ api.js
@@ -1,119 +1,129 @@
 module.exports = function (app, db) {
+  app.get('/api/test', function (req, res) {
+    res.json({
+      name: 'joe',
+    });
+  });
 
-	app.get('/api/test', function (req, res) {
-		res.json({
-			name: ''
-		});
-	});
+  app.get('/api/garments', async function (req, res) {
+    const {gender, season} = req.query;
+    let garments = [];
+    // add some sql queries that filter on gender & season
+    const conditions =
+      !gender && !season
+        ? ''
+        : `where ${gender ? `gender='${gender}'` : ''} ${
+            gender && season ? 'and' : ''
+          } ${season ? `season='${season}'` : ''}`;
 
-	app.get('/api/garments', async function (req, res) {
+    garments = await db.any(`select * from garment ${conditions}`);
 
-		const { gender, season } = req.query;
-		let garments = [];
-		// add some sql queries that filter on gender & season
+    res.json({
+      data: garments,
+    });
+  });
 
-		res.json({
-			data: garments
-		})
-	});
+  app.put('/api/garment/:id', async function (req, res) {
+    try {
+      // use an update query...
 
-	app.put('/api/garment/:id', async function (req, res) {
+      const {id} = req.params;
+      const data = {...req.body};
+      const fields = Object.keys(data);
+      let updates = [];
+      for (const field in data) {
+        if (fields.includes(field)) {
+          updates = [...updates, `${field} = '${data[field]}'`];
+        }
+      }
+      await db.none(`update garment set ${updates.toString()} where id=$1`, id);
 
-		try {
+      // const garment = await db.oneOrNone(`select * from garment where id = $1`, [id]);
 
-			// use an update query...
+      // you could use code like this if you want to update on any column in the table
+      // and allow users to only specify the fields to update
 
-			const { id } = req.params;
-			// const garment = await db.oneOrNone(`select * from garment where id = $1`, [id]);
+      // let params = { ...garment, ...req.body };
+      // const { description, price, img, season, gender } = params;
 
-			// you could use code like this if you want to update on any column in the table
-			// and allow users to only specify the fields to update
+      res.json({
+        status: 'success',
+      });
+    } catch (err) {
+      console.log(err);
+      res.json({
+        status: 'error',
+        error: err.message,
+      });
+    }
+  });
 
-			// let params = { ...garment, ...req.body };
-			// const { description, price, img, season, gender } = params;
+  app.get('/api/garment/:id', async function (req, res) {
+    try {
+      const {id} = req.params;
+      // get the garment from the database
+      const garment = await db.one(`select * from garment where id=$1`, id);
 
+      res.json({
+        status: 'success',
+        data: garment,
+      });
+    } catch (err) {
+      console.log(err);
+      res.json({
+        status: 'error',
+        error: err.message,
+      });
+    }
+  });
 
-			res.json({
-				status: 'success'
-			})
-		} catch (err) {
-			console.log(err);
-			res.json({
-				status: 'error',
-				error: err.message
-			})
-		}
-	});
+  app.post('/api/garment/', async function (req, res) {
+    try {
+      const {description, price, img, season, gender} = req.body;
 
-	app.get('/api/garment/:id', async function (req, res) {
+      // insert a new garment in the database
 
-		try {
-			const { id } = req.params;
-			// get the garment from the database
-			const garment = null;
+      await db.none(
+        'insert into garment (description, price, img, season, gender) values ($1,$2,$3,$4,$5) on conflict do nothing',
+        [description, price, img, season, gender],
+      );
 
-			res.json({
-				status: 'success',
-				data: garment
-			});
+      res.json({
+        status: 'success',
+      });
+    } catch (err) {
+      console.log(err);
+      res.json({
+        status: 'error',
+        error: err.message,
+      });
+    }
+  });
 
-		} catch (err) {
-			console.log(err);
-			res.json({
-				status: 'error',
-				error: err.message
-			})
-		}
-	});
+  app.get('/api/garments/grouped', async function (req, res) {
+    const result = await db.many(
+      `select gender, count(*) from garment group by gender order by count(*) asc`,
+    );
+    // use group by query with order by asc on count(*)
+    res.json({
+      data: result,
+    });
+  });
 
-
-	app.post('/api/garment/', async function (req, res) {
-
-		try {
-
-			const { description, price, img, season, gender } = req.body;
-
-			// insert a new garment in the database
-
-			res.json({
-				status: 'success',
-			});
-
-		} catch (err) {
-			console.log(err);
-			res.json({
-				status: 'error',
-				error: err.message
-			})
-		}
-	});
-
-	app.get('/api/garments/grouped', async function (req, res) {
-		const result = []		
-		// use group by query with order by asc on count(*)
-		res.json({
-			data: result
-		})
-	});
-
-
-	app.delete('/api/garments', async function (req, res) {
-
-		try {
-			const { gender } = req.query;
-			// delete the garments with the specified gender
-
-			res.json({
-				status: 'success'
-			})
-		} catch (err) {
-			// console.log(err);
-			res.json({
-				status: 'success',
-				error : err.stack
-			})
-		}
-	});
-
-
-}
+  app.delete('/api/garments', async function (req, res) {
+    try {
+      const {gender} = req.query;
+      // delete the garments with the specified gender
+      await db.none(`delete from garment where gender = $1`, gender);
+      res.json({
+        status: 'success',
+      });
+    } catch (err) {
+      // console.log(err);
+      res.json({
+        status: 'success',
+        error: err.stack,
+      });
+    }
+  });
+};
--- test/test.api.js
+++ test/test.api.js
@@ -1,16 +1,16 @@
 const supertest = require('supertest');
-const PgPromise = require("pg-promise")
+const PgPromise = require('pg-promise');
 const express = require('express');
 const assert = require('assert');
 const fs = require('fs');
-require('dotenv').config()
+require('dotenv').config();
 
 const API = require('../api');
-const { default: axios } = require('axios');
+const {default: axios} = require('axios');
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 
 const DATABASE_URL = process.env.DATABASE_URL;
 const pgp = PgPromise({});
@@ -19,208 +19,185 @@ const db = pgp(DATABASE_URL);
 API(app, db);
 
 describe('The Garment API', function () {
+  before(async function () {
+    this.timeout(5000);
+    await db.none(`delete from garment`);
+    const commandText = fs.readFileSync('./sql/data.sql', 'utf-8');
+    await db.none(commandText);
+  });
 
-	before(async function () {
-		this.timeout(5000);
-		await db.none(`delete from garment`);
-		const commandText = fs.readFileSync('./sql/data.sql', 'utf-8');
-		await db.none(commandText)
-	});
+  it('should have a test method', async () => {
+    const response = await supertest(app).get('/api/test').expect(200);
 
+    assert.deepStrictEqual({name: 'joe'}, response.body);
+  });
 
-	it('should have a test method', async () => {
+  it('should be able to find 30 garments', async () => {
+    const response = await supertest(app).get('/api/garments').expect(200);
 
-		const response = await supertest(app)
-			.get('/api/test')
-			.expect(200);
+    const garments = response.body.data;
+    assert.equal(30, garments.length);
+  });
 
-		assert.deepStrictEqual({ name: 'joe' }, response.body);
+  it('should be able to find all the Summer garments', async () => {
+    const response = await supertest(app)
+      .get('/api/garments?season=Summer')
+      .expect(200);
 
-	});
+    const garments = response.body.data;
+    assert.equal(12, garments.length);
+  });
 
-	it('should be able to find 30 garments', async () => {
-		const response = await supertest(app)
-			.get('/api/garments')
-			.expect(200);
+  it('should be able to find all the Winter garments', async () => {
+    // add some code below
 
-		const garments = response.body.data;
-		assert.equal(30, garments.length);
+    const response = await supertest(app)
+      .get('/api/garments?season=Winter')
+      .expect(200);
 
-	})
+    const garments = response.body.data;
+    assert.equal(5, garments.length);
+  });
 
-	it('should be able to find all the Summer garments', async () => {
-		const response = await supertest(app)
-			.get('/api/garments?season=Summer')
-			.expect(200);
+  it('should be able to find all the Winter Male garments', async () => {
+    // change the code statement below
 
-		const garments = response.body.data;
-		assert.equal(12, garments.length);
-	});
+    const response = await supertest(app)
+      .get('/api/garments?season=Winter&gender=Male')
+      .expect(200);
 
-	it('should be able to find all the Winter garments', async () => {
-		// add some code below
+    const garments = response.body.data;
+    assert.equal(3, garments.length);
+  });
 
-		const response = await supertest(app)
-			.get('/api/garments?season=Winter')
-			.expect(200);
+  it('you should be able to change a given Male garment to a Unisex garment', async () => {
+    const description = 'Red hooded jacket';
 
-		const garments = response.body.data;
-		assert.equal(5, garments.length);
-	});
+    // use db.one with an update sql statement
+    const garment_by_desc_sql = 'select * from garment where description = $1';
 
-	it('should be able to find all the Winter Male garments', async () => {
-		// change the code statement below
+    const garment = await db.one(garment_by_desc_sql, [description]);
+    assert.equal('Male', garment.gender);
 
-		const response = await supertest(app)
-			.get('/api/garments?season=Winter&gender=Male')
-			.expect(200);
+    await supertest(app)
+      .put(`/api/garment/${garment.id}`)
+      .send({gender: 'Unisex'})
+      .expect(200);
 
-		const garments = response.body.data;
-		assert.equal(3, garments.length);
+    //
+    const response = await supertest(app)
+      .get(`/api/garment/${garment.id}`)
+      .expect(200);
 
-	});
+    // console.log(response.body)
 
-	it('you should be able to change a given Male garment to a Unisex garment', async () => {
+    assert.equal('Unisex', response.body.data.gender);
+  });
 
-		const description = 'Red hooded jacket';
+  it('you should be able to add 2 Male & 3 Female garments', async () => {
+    // write your code above this line
 
-		// use db.one with an update sql statement
-		const garment_by_desc_sql = 'select * from garment where description = $1';
+    // const gender_count_sql = 'select count(*) from garment where gender = $1'
+    // const maleCount = await db.one(gender_count_sql, ['Male'], r => r.count);
+    // const femaleCount = await db.one(gender_count_sql, ['Female'], r => r.count);
 
-		const garment = await db.one(garment_by_desc_sql, [description]);
-		assert.equal('Male', garment.gender);
+    const maleResult = await supertest(app).get(`/api/garments?gender=Male`);
+    // console.log(maleResult.body.data);
 
-		await supertest(app)
-			.put(`/api/garment/${garment.id}`)
-			.send({ gender: 'Unisex' })
-			.expect(200);
+    assert.equal(13, maleResult.body.data.length);
 
-		//
-		const response = await supertest(app)
-			.get(`/api/garment/${garment.id}`)
-			.expect(200);
+    const femaleResult = await supertest(app).get(
+      `/api/garments?gender=Female`,
+    );
+    assert.equal(13, femaleResult.body.data.length);
 
-		// console.log(response.body)
+    await supertest(app).post('/api/garment').send({
+      gender: 'Female',
+      season: 'Winter',
+      description: 'Short winter boots',
+      price: 289.25,
+      img: 'placeholder.png',
+    });
 
-		assert.equal('Unisex', response.body.data.gender);
+    await supertest(app).post('/api/garment').send({
+      gender: 'Female',
+      season: 'Winter',
+      description: 'Long winter boots',
+      price: 289.25,
+      img: 'placeholder.png',
+    });
 
-	});
+    await supertest(app).post('/api/garment').send({
+      gender: 'Female',
+      season: 'Winter',
+      description: 'Long sleeve shirt',
+      price: 152.25,
+      img: 'placeholder.png',
+    });
 
-	it('you should be able to add 2 Male & 3 Female garments', async () => {
+    await supertest(app).post('/api/garment').send({
+      gender: 'Male',
+      season: 'Summer',
+      description: 'Short sleeve shirt',
+      price: 99.25,
+      img: 'placeholder.png',
+    });
+    await supertest(app).post('/api/garment').send({
+      gender: 'Male',
+      season: 'Summer',
+      description: 'Panama hat',
+      price: 252.25,
+      img: 'placeholder.png',
+    });
 
+    // went down 1 as the previous test is changing a male garment to a female one
+    const updatedMaleResult = await supertest(app).get(
+      `/api/garments?gender=Male`,
+    );
+    assert.equal(15, updatedMaleResult.body.data.length);
 
-		// write your code above this line
+    const updatedFemaleResult = await supertest(app).get(
+      `/api/garments?gender=Female`,
+    );
+    assert.equal(16, updatedFemaleResult.body.data.length);
+  });
 
-		// const gender_count_sql = 'select count(*) from garment where gender = $1'
-		// const maleCount = await db.one(gender_count_sql, ['Male'], r => r.count);
-		// const femaleCount = await db.one(gender_count_sql, ['Female'], r => r.count);
+  it('you should be able to group garments by gender and count them', async () => {
+    // and below this line for this function will
+    const result = await supertest(app)
+      .get(`/api/garments/grouped`)
+      .expect(200);
 
-		const maleResult = await supertest(app).get(`/api/garments?gender=Male`);
-		// console.log(maleResult.body.data);
+    const garmentsGrouped = result.body;
+    // console.log(garmentsGrouped);
 
-		assert.equal(13, maleResult.body.data.length)
+    // write your code above this line
+    const expectedResult = [
+      {
+        gender: 'Unisex',
+        count: '4',
+      },
+      {
+        gender: 'Male',
+        count: '15',
+      },
+      {
+        gender: 'Female',
+        count: '16',
+      },
+    ];
+    assert.deepStrictEqual(expectedResult, garmentsGrouped.data);
+  });
 
-		const femaleResult = await supertest(app).get(`/api/garments?gender=Female`);
-		assert.equal(13, femaleResult.body.data.length)
+  it('you should be able to remove all the Unisex garments', async () => {
+    await supertest(app).delete(`/api/garments?gender=Unisex`).expect(200);
 
-		await supertest(app)
-			.post('/api/garment')
-			.send({
-				gender: 'Female',
-				season: 'Winter',
-				description: 'Short winter boots',
-				price: 289.25,
-				img: 'placeholder.png'
-			});
+    // const unisexResults = await supertest(app).get(`/api/garments?gender=Unisex`);
+    // const unisexData = unisexResults.body
+    // assert.equal(0, unisexData.data.length);
+  });
 
-		await supertest(app)
-			.post('/api/garment')
-			.send({
-				gender: 'Female',
-				season: 'Winter',
-				description: 'Long winter boots',
-				price: 289.25,
-				img: 'placeholder.png'
-			});
-
-		await supertest(app)
-			.post('/api/garment')
-			.send({
-				gender: 'Female',
-				season: 'Winter',
-				description: 'Long sleeve shirt',
-				price: 152.25,
-				img: 'placeholder.png'
-			});
-
-		await supertest(app).post('/api/garment')
-			.send({
-				gender: 'Male',
-				season: 'Summer',
-				description: 'Short sleeve shirt',
-				price: 99.25,
-				img: 'placeholder.png'
-			});
-		await supertest(app).post('/api/garment')
-			.send({
-				gender: 'Male',
-				season: 'Summer',
-				description: 'Panama hat',
-				price: 252.25,
-				img: 'placeholder.png'
-			});
-
-		// went down 1 as the previous test is changing a male garment to a female one
-		const updatedMaleResult = await supertest(app).get(`/api/garments?gender=Male`);
-		assert.equal(15, updatedMaleResult.body.data.length);
-
-		const updatedFemaleResult = await supertest(app).get(`/api/garments?gender=Female`);
-		assert.equal(16, updatedFemaleResult.body.data.length);
-
-	});
-
-	it('you should be able to group garments by gender and count them', async () => {
-
-		// and below this line for this function will
-		const result = await supertest(app)
-				.get(`/api/garments/grouped`)
-				.expect(200);
-
-		const garmentsGrouped = result.body;
-		// console.log(garmentsGrouped);
-
-		// write your code above this line
-		const expectedResult = [
-			{
-				gender: 'Unisex',
-				count: '4'
-			},
-			{
-				gender: 'Male',
-				count: '15'
-			},
-			{
-				gender: 'Female',
-				count: '16'
-			}
-			
-		]
-		assert.deepStrictEqual(expectedResult, garmentsGrouped.data)
-	});
-
-	it('you should be able to remove all the Unisex garments', async () => {
-
-		await supertest(app)
-			.delete(`/api/garments?gender=Unisex`)
-			.expect(200);
-
-		// const unisexResults = await supertest(app).get(`/api/garments?gender=Unisex`);
-		// const unisexData = unisexResults.body
-		// assert.equal(0, unisexData.data.length);
-
-	});
-
-	after(() => {
-		db.$pool.end();
-	});
+  after(() => {
+    db.$pool.end();
+  });
 });
